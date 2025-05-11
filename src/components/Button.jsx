e8e43db4b896937ad8085a7eefa3c470f3726544
Elyasaf Cohen <Elyasaf93@gmail.com>
2025-05-11 22:18:46 +0300

    first tag for safety

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,12 +3,10 @@
 import React from 'react';
 import '../styles/Button.css';
 
-function Button({ label, onClick }) {
+export default function Button({ label, onClick }) {
   return (
     <button className="custom-button keyboard-button" onClick={onClick}>
       {label}
     </button>
   );
-}
-
-export default Button;
+};
